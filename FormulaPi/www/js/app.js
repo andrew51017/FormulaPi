7b650ff1452989008b6a3ff476849da126960f17
Andrew Fletcher <andrew.fletcher16@Gmail.com>
2014-06-04 22:06:12 +0100

    - Re-worked Ionic structure to match FormulaPi requirements.

--- FormulaPi/www/js/app.js
+++ FormulaPi/www/js/app.js
@@ -29,6 +29,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
+	.state('splash', {
+		url: "/splash", 
+		templateUrl: "templates/splash.html",
+		controller: 'SplashCtrl'
+	})
+  
     // setup an abstract state for the tabs directive
     .state('tab', {
       url: "/tab",
@@ -38,47 +44,18 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
     // Each tab has its own nav history stack:
 
-    .state('tab.dash', {
-      url: '/dash',
+    .state('tab.rc', {
+      url: '/rc',
       views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
-        }
-      }
-    })
-
-    .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
-
-    .state('tab.account', {
-      url: '/account',
-      views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
+        'tab-rc': {
+          templateUrl: 'templates/tab-rc.html',
+          controller: 'RcCtrl'
         }
       }
     })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/splash');
 
 });
 
--- FormulaPi/www/js/controllers.js
+++ FormulaPi/www/js/controllers.js
@@ -1,15 +1,7 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('RcCtrl', function($scope) {
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
+.controller('SplashCtrl', function($scope) {
 });
